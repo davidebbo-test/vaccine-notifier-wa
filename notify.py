4471a611087dd92ad9fb480b26c20e73e87061a0
laveeshr <lrohra@asu.edu>
2021-04-07 19:52:03 -0700

    Test when nothing found

--- notify.py
+++ notify.py
@@ -8,7 +8,7 @@ import requests as requests
 def check_availability():
     url = "https://prepmod.doh.wa.gov/clinic/search?q[services_name_in][]=covid&q[age_groups_name_in][" \
           "]=Adults&location=98004&search_radius=10+miles&q[venue_search_name_or_venue_name_i_cont]=&clinic_date_eq[" \
-          "year]=2021&clinic_date_eq[month]=4&clinic_date_eq[day]=10&q[" \
+          "year]=2021&clinic_date_eq[month]=4&clinic_date_eq[day]=28&q[" \
           "vaccinations_name_i_cont]=Moderna&commit=Search#search_results "
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
